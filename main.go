a50e529b3cf66e72e6d1bb4ee1150416f4a7bb9d
Jormen Janssen <j.janssen@riwo.eu>
2018-10-11 14:02:15 +0200

    Generate http traffic

--- main.go
+++ main.go
@@ -2,15 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net"
+	"net/http"
+	"os"
 	"strings"
 	"time"
 )
 
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 
-	domains, _ := getDomains()
+	log.SetOutput(os.Stdout)
+
+	err := DownloadFile("random-domains.txt", "https://github.com/jormenjanssen/cmmfo/raw/master/random-domains.txt")
+	if err != nil {
+		log.Fatalf("failed to download domain file: %v", err)
+	}
+
+	domains, err := GetDomains()
+	if err != nil {
+		log.Fatalf("failed to read/parse domains file: %v", err)
+	}
 
 	for {
 
@@ -18,19 +59,29 @@ func main() {
 			_, err := net.LookupHost(url)
 
 			if err != nil {
-				fmt.Println(fmt.Sprintf("Cannot resolve host: %v error: %v", url, err))
+				log.Println(fmt.Sprintf("Cannot resolve host: %v error: %v", url, err))
 			} else {
-				fmt.Println(fmt.Sprintf("Resolved host: %v", url))
+				log.Println(fmt.Sprintf("Resolved host: %v", url))
+				go func() {
+					// Visiting domain
+					verr := VisitDomain(fmt.Sprintf("http://%v", url))
+					if verr != nil {
+						log.Println(fmt.Sprintf("Failed to get request(http://%v) %v", url, verr))
+					} else {
+						log.Println(fmt.Sprintf("Succesfull visited (http://%v)", url))
+					}
+				}()
 			}
 
-			time.Sleep(170 * time.Millisecond)
+			time.Sleep(270 * time.Millisecond)
 		}
 
-		time.Sleep(7 * time.Minute)
+		time.Sleep(6 * time.Minute)
 	}
 }
 
-func getDomains() ([]string, error) {
+// GetDomains returns a long list of Domain from the domains.txt file
+func GetDomains() ([]string, error) {
 
 	content, err := ioutil.ReadFile("random-domains.txt")
 
@@ -40,4 +91,19 @@ func getDomains() ([]string, error) {
 
 	lines := strings.Split(string(content), "\n")
 	return lines, nil
+}
+
+// VisitDomain visits a http domain
+func VisitDomain(url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	buffer := make([]byte, 4096)
+	_, err = resp.Body.Read(buffer)
+	return err
 }
